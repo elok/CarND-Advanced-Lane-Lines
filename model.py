298fb50b3c6a53c531c234dec8c62b0bbe6a795b
ericlok <ericlok@gmail.com>
2017-11-23 23:11:31 -0500

    adding changes to process video

--- model.py
+++ model.py
@@ -18,6 +18,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# Define a class to receive the characteristics of each line detection
+class Line():
+    """
+    Class to keep track of things like where your last several detections of the lane lines were and what
+    the curvature was, so you can properly treat new detections
+    """
+    def __init__(self):
+        # was the line detected in the last iteration?
+        self.detected = False
+        # x values of the last n fits of the line
+        self.recent_xfitted = []
+        # average x values of the fitted line over the last n iterations
+        self.bestx = None
+        # polynomial coefficients averaged over the last n iterations
+        self.best_fit = None
+        # polynomial coefficients for the most recent fit
+        self.current_fit = [np.array([False])]
+        # radius of curvature of the line in some units
+        self.radius_of_curvature = None
+        # distance in meters of vehicle center from the line
+        self.line_base_pos = None
+        # difference in fit coefficients between last and new fits
+        self.diffs = np.array([0,0,0], dtype='float')
+        # x values for detected line pixels
+        self.allx = None
+        # y values for detected line pixels
+        self.ally = None
+
+
 def calibrate_camera(path, img):
     """
     Chessboard size 9x6
@@ -352,9 +381,71 @@ def find_lane(binary_warped):
 
     return left_fitx, right_fitx, ploty
 
+def process_image(img_data):
+    """
 
-def run():
+    :param img_data:
+    :return:
+    """
+    # Calibrate the camera (matrix)
+    top_down, perspective_M, undist = calibrate_camera(r'camera_cal/', img_data)
 
+    binary = pipeline(img_data)
+
+    # plt.imshow(cv2.cvtColor(binary, cv2.COLOR_BGR2RGB))
+    # plt.imshow(binary)
+    # plt.show()
+
+    # Perspective Transform
+    binary_warped, Minv = perspective_transform(binary)
+    r, g, b = cv2.split(binary_warped)
+
+    # plt.imshow(g)
+    # plt.show()
+
+    # Find lane lines
+    left_fitx, right_fitx, ploty = find_lane(b)
+
+    # Plot the result
+    # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
+    # f.tight_layout()
+    # ax1.imshow(cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB))
+    # ax1.set_title('Original Image', fontsize=40)
+    # # ax2.imshow(cv2.cvtColor(warped, cv2.COLOR_BGR2RGB))
+    # ax2.imshow(g)
+    # ax2.set_title('Pipeline Result', fontsize=40)
+    # plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
+    # plt.show()
+
+    # -----------------------------------------------------------------------------
+    # OVERLAY
+    # -----------------------------------------------------------------------------
+
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(g).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, Minv, (img_data.shape[1], img_data.shape[0]))
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+
+    plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
+
+
+def run_on_test_images():
+    """
+
+    :return:
+    """
     path = r'test_images/'
 
     for image_file_name in os.listdir(path):
@@ -368,60 +459,10 @@ def run():
         # Read image using opencv
         img_data = cv2.imread(os.path.join(path, image_file_name))  # BGR
 
-        # Calibrate the camera (matrix)
-        top_down, perspective_M, undist = calibrate_camera(r'camera_cal/', img_data)
+        process_image(img_data)
 
-        binary = pipeline(img_data)
+        break
 
-        # plt.imshow(cv2.cvtColor(binary, cv2.COLOR_BGR2RGB))
-        # plt.imshow(binary)
-        # plt.show()
-
-        # Perspective Transform
-        binary_warped, Minv = perspective_transform(binary)
-        r, g, b = cv2.split(binary_warped)
-
-        # plt.imshow(g)
-        # plt.show()
-
-        # Find lane lines
-        left_fitx, right_fitx, ploty = find_lane(b)
-
-        # Plot the result
-        # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-        # f.tight_layout()
-        # ax1.imshow(cv2.cvtColor(img_data, cv2.COLOR_BGR2RGB))
-        # ax1.set_title('Original Image', fontsize=40)
-        # # ax2.imshow(cv2.cvtColor(warped, cv2.COLOR_BGR2RGB))
-        # ax2.imshow(g)
-        # ax2.set_title('Pipeline Result', fontsize=40)
-        # plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-        # plt.show()
-
-        # -----------------------------------------------------------------------------
-        # OVERLAY
-        # -----------------------------------------------------------------------------
-
-        # Create an image to draw the lines on
-        warp_zero = np.zeros_like(g).astype(np.uint8)
-        color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
-
-        # Recast the x and y points into usable format for cv2.fillPoly()
-        pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
-        pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
-        pts = np.hstack((pts_left, pts_right))
-
-        # Draw the lane onto the warped blank image
-        cv2.fillPoly(color_warp, np.int_([pts]), (0, 255, 0))
-
-        # Warp the blank back to original image space using inverse perspective matrix (Minv)
-        newwarp = cv2.warpPerspective(color_warp, Minv, (img_data.shape[1], img_data.shape[0]))
-        # Combine the result with the original image
-        result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
-
-        plt.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
-
-        pass
 
 if __name__ == '__main__':
-    run()
+    run_on_test_images()
