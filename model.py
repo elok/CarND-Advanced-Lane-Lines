301f41dd473dc359e3b6c73d5318333dd10a04d2
elok <eric.lok@secor-am.com>
2017-11-28 08:40:34 -0500

    removed unused code

--- model.py
+++ model.py
@@ -259,36 +259,6 @@ def find_lane_histogram(binary_warped):
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
 
-    # -----------------------------------------------------------------
-    # Visualization
-    # -----------------------------------------------------------------
-
-    # Generate x and y values for plotting
-    # ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
-    # left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-    # right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
-
-    # out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-    # out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-    # plt.imshow(out_img)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
-
-    # Plot the result
-    # f, (ax1, ax2) = plt.subplots(1, 2, figsize=(24, 9))
-    # f.tight_layout()
-    # ax1.imshow(binary_warped)
-    # ax1.set_title('Binary Warped', fontsize=40)
-    # ax2.imshow(out_img)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
-    # ax2.set_title('Histogram', fontsize=40)
-    # plt.show()
-
     return left_fit, right_fit
 
 def find_lane_using_previous(binary_warped):
@@ -328,41 +298,6 @@ def find_lane_using_previous(binary_warped):
     # Fit a second order polynomial to each
     left_fit = np.polyfit(lefty, leftx, 2)
     right_fit = np.polyfit(righty, rightx, 2)
-
-    # -----------------------------------------------------------------
-    # Visualization
-    # -----------------------------------------------------------------
-
-    # Generate x and y values for plotting
-    # ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
-    # left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
-    # right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
-
-    # # Create an image to draw on and an image to show the selection window
-    # out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
-    # window_img = np.zeros_like(out_img)
-    # # Color in left and right line pixels
-    # out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
-    # out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
-
-    # Generate a polygon to illustrate the search window area
-    # And recast the x and y points into usable format for cv2.fillPoly()
-    # left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
-    # left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
-    # left_line_pts = np.hstack((left_line_window1, left_line_window2))
-    # right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
-    # right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
-    # right_line_pts = np.hstack((right_line_window1, right_line_window2))
-
-    # Draw the lane onto the warped blank image
-    # cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
-    # cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
-    # result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
-    # plt.imshow(result)
-    # plt.plot(left_fitx, ploty, color='yellow')
-    # plt.plot(right_fitx, ploty, color='yellow')
-    # plt.xlim(0, 1280)
-    # plt.ylim(720, 0)
 
     return left_fit, right_fit
 
@@ -504,22 +439,6 @@ def draw_lane_lines(img_data):
 
     # Find lane lines
     left_fitx, right_fitx, ploty = find_lane(g)
-
-    # # -----------------------------------------------------------------
-    # # HANDLING
-    # # -----------------------------------------------------------------
-    # if (not any(left_fitx)) or (not any(right_fitx)):
-    #     # Try the B channel
-    #     left_fitx, right_fitx, ploty = find_lane(b)
-    #
-    #     if (not any(left_fitx)) or (not any(right_fitx)):
-    #         # visualize(undist, binary_warped)
-    #
-    #         # TODO: sfds
-    #
-    #         # use last lanes
-    #         left_fitx = line_left_data.recent_xfitted
-    #         right_fitx = line_right_data.recent_xfitted
 
     # -----------------------------------------------------------------
     # OVERLAY
