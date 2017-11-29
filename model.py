9f2743ecb1dc640a712fe13edfd5b7d1c6f93e77
elok <eric.lok@secor-am.com>
2017-11-29 17:47:49 -0500

    adding picture in picture

--- model.py
+++ model.py
@@ -92,7 +92,7 @@ class Blah():
         # Using the camera matrix and distortion coeff, undistort the image
         undist = cv2.undistort(src=img_data, cameraMatrix=mtx, distCoeffs=dist, newCameraMatrix=None, dst=mtx)
 
-        binary = generate_binary(img_data)
+        binary = generate_binary(undist)
 
         # Warp an image using the perspective transform, M:
         binary_warped = cv2.warpPerspective(src=binary, M=M, dsize=IMG_SIZE, flags=cv2.INTER_LINEAR)
@@ -144,6 +144,20 @@ class Blah():
         # Overlay
         # small = cv2.resize(g, (0, 0), fx=0.3, fy=0.3)
         # new_result = cv2.addWeighted(small, 0.7, result, 0.3, 0)
+
+        visualize_lane_lines(g, self.line_left_data.current_fit, self.line_right_data.current_fit)
+        binary_warped_with_lines = cv2.imread('temp.png')
+
+        binary_warped_thumbnail = cv2.resize(binary_warped, (0, 0), fx=0.3, fy=0.3)
+        binary_warped_with_lines_thumbnail = cv2.resize(binary_warped_with_lines, (0, 0), fx=0.3, fy=0.3)
+
+        x_offset = 800
+        y_offset = 25
+        result[y_offset:y_offset + binary_warped_thumbnail.shape[0], x_offset:x_offset + binary_warped_thumbnail.shape[1]] = binary_warped_thumbnail
+        y_offset = 200
+        result[y_offset:y_offset + binary_warped_with_lines_thumbnail.shape[0], x_offset:x_offset + binary_warped_with_lines_thumbnail.shape[1]] = binary_warped_with_lines_thumbnail
+        plt.imshow(result)
+        plt.show()
 
         return result
 
@@ -402,6 +416,63 @@ def visualize(img_data, img_data_2):
     plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
     plt.show()
 
+def visualize_lane_lines(binary_warped, left_fit, right_fit):
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    margin = 100
+
+    left_lane_inds = ((nonzerox > (left_fit[0] * (nonzeroy ** 2) + left_fit[1] * nonzeroy +
+                                   left_fit[2] - margin)) & (nonzerox < (left_fit[0] * (nonzeroy ** 2) +
+                                                                         left_fit[1] * nonzeroy + left_fit[
+                                                                             2] + margin)))
+
+    right_lane_inds = ((nonzerox > (right_fit[0] * (nonzeroy ** 2) + right_fit[1] * nonzeroy +
+                                    right_fit[2] - margin)) & (nonzerox < (right_fit[0] * (nonzeroy ** 2) +
+                                                                           right_fit[1] * nonzeroy + right_fit[
+                                                                               2] + margin)))
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, binary_warped.shape[0] - 1, binary_warped.shape[0])
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+
+    # Create an image to draw on and an image to show the selection window
+    out_img = np.dstack((binary_warped, binary_warped, binary_warped)) * 255
+    window_img = np.zeros_like(out_img)
+    # Color in left and right line pixels
+    out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [0, 0, 255]
+
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin,
+                                                                    ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin,
+                                                                     ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+    result = cv2.addWeighted(out_img, 1, window_img, 0.3, 0)
+    # plt.imshow(result)
+    # plt.plot(left_fitx, ploty, color='yellow')
+    # plt.plot(right_fitx, ploty, color='yellow')
+    # plt.xlim(0, 1280)
+    # plt.ylim(720, 0)
+    fig = plt.figure(frameon=False)
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+    ax.imshow(result)
+    ax.plot(left_fitx, ploty, color='yellow')
+    ax.plot(right_fitx, ploty, color='yellow')
+    fig.savefig('temp.png')
+
 def calibrate_camera(path):
     """
     Chessboard size 9x6
@@ -634,10 +705,10 @@ def run_on_test_images():
         ax2.imshow(cv2.cvtColor(result, cv2.COLOR_BGR2RGB))
         ax2.set_title('Pipeline Result', fontsize=12)
         plt.subplots_adjust(left=0., right=1, top=0.9, bottom=0.)
-        # plt.show()
+        plt.show()
 
         # Save image
-        cv2.imwrite(os.path.join(r'output_images/', image_file_name), result)  # BGR
+        # cv2.imwrite(os.path.join(r'output_images/', image_file_name), result)  # BGR
 
 def run_on_video():
     calibrate_camera_and_pers_transform()
